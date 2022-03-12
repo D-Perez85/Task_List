ef5fe9a7e235ae78365983ae028de2963b5babb3
D-Perez85 <77124855+D-Perez85@users.noreply.github.com>
2022-03-12 19:58:39 -0300

    Cascaron JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
-import './App.css';
+import React from "react"; 
+import "./styles.css";
 
-function App() {
+
+const App = ()=> {
   return (
-    <div className="App">
-     <h3>APP USE REDUCER</h3>
+    <div>
+      <h1>Lista de Tareas </h1>
+      <hr />
+
+      <div className="row">
+        <div className="col-7">
+          <ul className="list-group list-group-flush">
+           <li>A</li>
+           <li>B</li>
+           <li>C</li>
+          </ul>
+        </div>
+        <div className="col-5">
+            <h4>Agregar Tarea</h4>
+            <hr />
+             <form>
+                <input type="text" name="description" placeholder="Ingrese la tarea pendiente..." className="form-control" autoComplete="off" />
+                <button type="submit" className="btn btn-outline-primary margin-top">Agregar</button>
+            </form>
+            </div>
+      </div>
     </div>
   );
 }
