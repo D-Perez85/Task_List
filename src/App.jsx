eb7535c53be3eec059f58fce99871d52a906271f
D-Perez85 <77124855+D-Perez85@users.noreply.github.com>
2022-03-12 21:38:05 -0300

    Componente AddTarea - case: add

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useReducer } from "react";
+import { AddTarea } from "./components/AddTarea";
 import { useForm } from "./hooks/useForm";
 import { reducer } from "./reducer";
 import "./styles.css";
@@ -8,29 +9,18 @@ const App = () => {
     return JSON.parse(localStorage.getItem("tareas")) || [];
   };
   const [tareas, dispatch] = useReducer(reducer, [], init);
-  const [{ description }, handleChange, reset] = useForm({
-    description: "",
-  });
 
   useEffect(() => {
     localStorage.setItem("tareas", JSON.stringify(tareas));
   }, [tareas]);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    const newTarea = {
-      id: new Date().getTime(),
-      desc: description,
-      done: false,
-    };
-    const action = {
-      type: "add",
-      payload: newTarea,
-    };
-    dispatch(action);
-    reset();
-  };
+  const handleAddTarea = (newTarea) =>{
+    dispatch({
+     type: "add",
+     payload: newTarea,
+   });
+  }; 
+  
   const handleDelete = (id) => {
     const action = {
       type: "delete",
@@ -59,14 +49,11 @@ const App = () => {
                 <li className="list-group-item">
                   <p
                     className={`${value.done && "complete"}`}
-                    onClick={() => handleToggle(value.id)}
-                  >
-                    {index + 1} - {value.desc}
-                  </p>
+                    onClick={() => handleToggle(value.id)}>
+                    {index + 1} - {value.desc} </p>
                   <button
                     className="btn btn-danger"
-                    onClick={() => handleDelete(value.id)}
-                  >
+                    onClick={() => handleDelete(value.id)}>
                     Borrar
                   </button>
                 </li>
@@ -75,25 +62,7 @@ const App = () => {
           </ul>
         </div>
         <div className="col-5">
-          <h4>Agregar Tarea</h4>
-          <hr />
-          <form onSubmit={handleSubmit}>
-            <input
-              type="text"
-              name="description"
-              onChange={handleChange}
-              value={description}
-              placeholder="Ingrese aqui su tarea..."
-              className="form-control"
-              autoComplete="off"
-            />
-            <button
-              type="submit"
-              className="btn btn-outline-primary margin-top"
-            >
-              Agregar
-            </button>
-          </form>
+           <AddTarea handleAddTarea={handleAddTarea}/>
         </div>
       </div>
     </div>
---
+++ src/components/AddTarea.jsx
@@ -0,0 +1,43 @@
+import React from "react"; 
+import { useForm } from "../hooks/useForm";
+
+export const AddTarea = ({handleAddTarea}) =>{
+   const[{description}, handleChange, reset] =  useForm({
+    description: '',
+}); 
+
+const handleSubmit = (e) => {
+    e.preventDefault();
+    if (description.trim().length <= 1) {
+      return;
+    }
+    const newTarea = {
+      id: new Date().getTime(),
+      desc: description,
+      done: false,
+    };
+    handleAddTarea(newTarea);
+    reset();
+  };
+
+  return (
+    <>
+      <h4>Agregar Tarea</h4>
+      <hr />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          onChange={handleChange}
+          value={description}
+          name="description"
+          className="form-control"
+          placeholder="Ingrese aqui su tarea..."
+
+        />
+        <button type="submit" className="btn btn-outline-primary margin-top">
+          Agregar
+        </button>
+      </form>
+    </>
+  );
+}; 
